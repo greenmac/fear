c5b2ba2af9b54324489d7ffc339318976dde1e0e
greenmac <alwaysmac@msn.com>
2018-08-21 21:53:02 +0800

    48:00

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,16 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Todo;
 
 class TodoController extends Controller
 {
    public function index()
    {
-       return view('todo.index');
+        $todos = Todo::all();
+        // dd($todos);
+
+       return view('todo.index',[
+           'todos' => $todos
+       ]);
        // view('view下面的檔案,如果有再建立一個資料夾,變成 資料夾.檔案名')
    }
-   public function update()
+   public function update(Request $request)
    {
-       return '123';
-   } 
+    #第一種儲存方式
+    //    $todo = new Todo();
+    //    $todo->title = $request->title;
+    //    $todo->save(); 
+
+    #第二種儲存方式
+        // $todo = Todo::create([
+        //     'title' => $request->title,
+        // ]);
+
+    #第三種儲存方式
+        $todo = Todo::create($request->all());
+
+        return redirect('todo');
+   }   
 }
--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -1,5 +1,11 @@
+<!-- {{ $todos }} -->
+
+@foreach ($todos as $todo)
+    <p>{{ $todo->id . '.' . $todo->title }}</p>
+@endforeach
+
 <form action="/todo" method="POST">
     {{ csrf_field() }}
-    <input type="text" placeholder="請輸入東西">
+    <input type="text" placeholder="請輸入東西" name="title">
     <input type="submit">
 </form>
