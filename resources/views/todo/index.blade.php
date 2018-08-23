a63b73aeb7f79aa2cf1b5c6eda1a40b57ceae6d3
greenmac <alwaysmac@msn.com>
2018-08-23 22:16:38 +0800

    End

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -5,15 +5,16 @@
 @foreach ($todos as $todo)
     <p>
         {{ $todo->id . '.' . $todo->title }}
-        <form action="todo/{{ $todo->id }}" method="POST">
+        <form action='{{ url("todo/{$todo->id}") }}' method="POST">
+        <!-- <form action="todo/{{ $todo->id }}" method="POST"> -->
             {{ csrf_field() }}
             {!! method_field('delete') !!}
             <input type="submit" value="Delete">
         </form>
     </p>
 @endforeach
-
-<form action="/todo" method="POST">
+<form action="{{ url('todo') }}" method="POST">
+<!-- <form action="/todo" method="POST"> -->
     {{ csrf_field() }}
     <input type="text" placeholder="請輸入東西" name="title">
     <input type="submit">
